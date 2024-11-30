e61a026832b7d096159109a054d3efaab71e8b57
Asumi Esmeralda Peñafiel Villafuerte <asumi.penafiel.villafuerte@vallegrande.edu.pe>
2024-11-30 02:09:18 -0500

    Delete public directory

--- public/js/responsiMenu.js
+++
@@ -1,4 +0,0 @@
-function toggleMenu() {
-    const menu = document.getElementById("menuOffCanvas");
-    menu.classList.toggle("activo");  // Agrega o quita la clase 'activo' para mostrar u ocultar el menú
-}
